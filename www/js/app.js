13cb5c01e9d71a1246713fb10291019d15c6a0bd
kingstakh <kingstakh@yandex.ru>
2016-06-21 11:28:47 +0300

    Update app.js

--- www/js/app.js
+++ www/js/app.js
@@ -12,21 +12,21 @@ angular.module('wpIonic', ['ionic','ionic.service.core', 'wpIonic.controllers', 
     // window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
     if (window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    };
+    }
     if (window.StatusBar) {
       // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
-    };
+    }
     
     var notificationOpenedCallback = function(jsonData) {
       alert("Notification received:\n" + JSON.stringify(jsonData));
       console.log('didReceiveRemoteNotificationCallBack: ' + JSON.stringify(jsonData));
-    };
+    }
     
     // Update with your OneSignal AppId and googleProjectNumber before running.
     window.plugins.OneSignal.init("a35c1178-2390-4c70-866c-78d93cffc4be",
                                    {googleProjectNumber: "281905587763"},
-                                   notificationOpenedCallback);
+                                   notificationOpenedCallback)
   });
 })
 
