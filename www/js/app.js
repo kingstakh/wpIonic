94b86458762cce21384f09188d9bfd61dadb6748
kingstakh <kingstakh@yandex.ru>
2016-06-21 11:37:50 +0300

    Update app.js

--- www/js/app.js
+++ www/js/app.js
@@ -137,20 +137,3 @@ function handleOpenURL(url) {
         window.dispatchEvent(event);
     }, 0);
 }
-
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {    
-    // Enable to debug issues.
-    // window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
-    
-    var notificationOpenedCallback = function(jsonData) {
-      alert("Notification received:\n" + JSON.stringify(jsonData));
-      console.log('didReceiveRemoteNotificationCallBack: ' + JSON.stringify(jsonData));
-    };
-    
-    // Update with your OneSignal AppId and googleProjectNumber before running.
-    window.plugins.OneSignal.init("a35c1178-2390-4c70-866c-78d93cffc4be",
-                                   {googleProjectNumber: "281905587763"},
-                                   notificationOpenedCallback);
-  });
-})
