dbaf2eb246f018c31c2cc9a56753ecce08d91b5c
kingstakh <kingstakh@yandex.ru>
2016-06-23 22:34:36 +0300

    Update controllers.js

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -100,20 +100,24 @@ angular.module('wpIonic.controllers', [])
 
 .controller('PostsCtrl', function( $scope, $http, DataLoader, $ionicLoading, $timeout, $ionicSlideBoxDelegate, $rootScope, $log) {
 
-    // Check for network connection
-    if(window.Connection) {
-      if(navigator.connection.type == Connection.NONE) {
-        $ionicPopup.confirm({
-          title: 'No Internet Connection',
-          content: 'Sorry, no Internet connectivity detected. Please reconnect and try again.'
-        })
-        .then(function(result) {
-          if(!result) {
-            ionic.Platform.exitApp();
-          }
-        });
-      }
-    }
+    // Check if is Offline
+    document.addEventListener("offline", function(){
+
+      offlineMessage.show();
+
+      /* 
+       * With this line of code you can hide the modal in 8 seconds but the user will be able to use your app
+       * If you want to block the use of the app till the user gets internet again, please delete this line.       
+       */
+
+      setTimeout('offlineMessage.hide()', 8000);  
+
+    }, false);
+
+    document.addEventListener("online", function(){
+      // If you remove the "setTimeout('offlineMessage.hide()', 8000);" you must remove the comment for the line above      
+      // offlineMessage.hide();
+    });
 
   var postsApi = $rootScope.url + 'posts';
 
