8c248502622ba01a70ccf78401f39fd598f7ac31
kingstakh <kingstakh@yandex.ru>
2016-06-24 12:29:51 +0300

    Update services.js

--- www/js/services.js
+++ www/js/services.js
@@ -14,6 +14,29 @@ angular.module('wpIonic.services', [])
 
 })
 
+// Check network connection
+.factory('onlineStatus', ["$window", "$rootScope", function ($window, $rootScope) {
+    var onlineStatus = {};
+
+    onlineStatus.onLine = $window.navigator.onLine;
+
+    onlineStatus.isOnline = function() {
+        return onlineStatus.onLine;
+    }
+
+    $window.addEventListener("online", function () {
+        onlineStatus.onLine = true;
+        $rootScope.$digest();
+    }, true);
+
+    $window.addEventListener("offline", function () {
+        onlineStatus.onLine = false;
+        $rootScope.$digest();
+    }, true);
+
+    return onlineStatus;
+}])
+
 .factory('Bookmark', function( CacheFactory ) {
 
   if ( ! CacheFactory.get('bookmarkCache') ) {
