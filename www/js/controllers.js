1c452bee4de28bf2a78ab8b0e718c5e75dd57cca
kingstakh <kingstakh@yandex.ru>
2016-06-24 12:01:13 +0300

    Update controllers.js

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -9,33 +9,6 @@ angular.module('wpIonic.controllers', [])
 
 })
 
-.controller('NetworkCtrl', function($scope, $cordovaNetwork, $rootScope) {
-    document.addEventListener("deviceready", function () {
-
-        $scope.network = $cordovaNetwork.getNetwork();
-        $scope.isOnline = $cordovaNetwork.isOnline();
-        $scope.$apply();
-
-        // listen for Online event
-        $rootScope.$on('$cordovaNetwork:online', function(event, networkState){
-            $scope.isOnline = true;
-            $scope.network = $cordovaNetwork.getNetwork();
-
-            $scope.$apply();
-        })
-
-        // listen for Offline event
-        $rootScope.$on('$cordovaNetwork:offline', function(event, networkState){
-            console.log("got offline");
-            $scope.isOnline = false;
-            $scope.network = $cordovaNetwork.getNetwork();
-
-            $scope.$apply();
-        })
-
-  }, false);
-});
-
 .controller('PostCtrl', function($scope, $stateParams, DataLoader, $ionicLoading, $rootScope, $sce, CacheFactory, $log, Bookmark, $timeout ) {
 
   if ( ! CacheFactory.get('postCache') ) {
@@ -126,7 +99,7 @@ angular.module('wpIonic.controllers', [])
 })
 
 .controller('PostsCtrl', function( $scope, $http, DataLoader, $ionicLoading, $timeout, CacheFactory, $ionicSlideBoxDelegate, $rootScope, $log) {
-  
+$scope.$watch('online', function(newStatus) { ... });  
   var postsApi = $rootScope.url + 'posts';
 
   $scope.moreItems = false;
