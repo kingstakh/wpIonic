073bc2d182b51d6f879f76adbffcc28a98535d3d
kingstakh <kingstakh@yandex.ru>
2016-06-29 16:11:58 +0300

    Update controllers.js

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -4,7 +4,7 @@ angular.module('wpIonic.controllers', [])
   
   // Enter your site url here. You must have the WP-API v2 installed on this site. Leave /wp-json/wp/v2/ at the end.
   $rootScope.url = 'http://1871.by/wp-json/wp/v2/';
-
+window.plugins.toast.show('Sorry, no data response. Please verify your connection and pull to refresh page.', 'long', 'center', function(a){}, function(b){});
    // $rootScope.callback = '_jsonp=JSON_CALLBACK';
 
 })
@@ -49,11 +49,7 @@ angular.module('wpIonic.controllers', [])
       $timeout( function() {
         
         $ionicLoading.hide();
-        
-        window.plugins.toast.showShortCenter(
-          "Sorry, no data response. Please verify your connection and pull to refresh page.", "4000", function(a){}, function(b){}
-        );
-      
+        window.plugins.toast.show('Sorry, no data response. Please verify your connection and pull to refresh page.', 'long', 'center', function(a){}, function(b){});
     }, 8000);
     });
 
