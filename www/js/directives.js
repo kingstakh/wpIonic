5041a85f28fc79146e4166030a6b761987c64b6c
kingstakh <kingstakh@yandex.ru>
2016-07-04 11:40:02 +0300

    Create directives.js

---
+++ www/js/directives.js
@@ -0,0 +1,49 @@
+angular.module('wpIonic.directives', [])
+
+/**
+ * A simple example directive.
+ */
+ 
+ .directive('biggerText', function($ionicGesture) {
+	return {
+		restrict: 'A',
+		link: function(scope, element, attrs) {
+			$ionicGesture.on('touch', function(event){
+				event.stopPropagation();
+				event.preventDefault();
+
+				var text_element = document.querySelector(attrs.biggerText),
+						root_element = document.querySelector(".menu-content"),
+						current_size_str = window.getComputedStyle(text_element, null).getPropertyValue('font-size'),
+						current_size = parseFloat(current_size_str),
+						new_size = Math.min((current_size+2), 24),
+						new_size_str = new_size + 'px';
+
+				root_element.classList.remove("post-size-"+current_size_str);
+				root_element.classList.add("post-size-"+new_size_str);
+			}, element);
+		}
+	};
+})
+
+.directive('smallerText', function($ionicGesture) {
+	return {
+		restrict: 'A',
+		link: function(scope, element, attrs) {
+			$ionicGesture.on('touch', function(event){
+				event.stopPropagation();
+				event.preventDefault();
+
+				var text_element = document.querySelector(attrs.smallerText),
+				root_element = document.querySelector(".menu-content"),
+				current_size_str = window.getComputedStyle(text_element, null).getPropertyValue('font-size'),
+				current_size = parseFloat(current_size_str),
+				new_size = Math.max((current_size-2), 12),
+				new_size_str = new_size + 'px';
+
+				root_element.classList.remove("post-size-"+current_size_str);
+				root_element.classList.add("post-size-"+new_size_str);
+			}, element);
+		}
+	};
+})
